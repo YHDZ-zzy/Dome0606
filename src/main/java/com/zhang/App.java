41516bf0c1575b3b03637e4da3b22c209f944dc0
Spica0307 <lil9114.2@gmail.com>
2023-06-06 19:53:45 +0800

    miraca修改了

--- src/main/java/com/zhang/App.java
+++ src/main/java/com/zhang/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println("123345");
-        System.out.println( "Hello World!" );
         System.out.println("miraca修改了app");
     }
 }
