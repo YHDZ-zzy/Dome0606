94be2c93012e820bd9ef949cbe28040559040e7e
ZZY <zhang187836@163.com>
2023-06-06 19:46:24 +0800

    3

--- src/main/java/com/zhang/App.java
+++ src/main/java/com/zhang/App.java
@@ -6,8 +6,8 @@ package com.zhang;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        System.out.println("123345");
         System.out.println( "Hello World!" );
     }
 }
